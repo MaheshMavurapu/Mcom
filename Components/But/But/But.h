43ee96f9ad2a4d29951f2ca03318a8ed7ba6873f
Mahesh Mavurapu <maheshmavurapu@Maheshs-MacBook-Pro.local>
2018-12-19 23:13:20 +0530

    Soruce files to But

---
+++ Components/But/But/But.h
@@ -0,0 +1,19 @@
+//
+//  But.h
+//  But
+//
+//  Created by Mahesh Mavurapu on 19/12/18.
+//  Copyright © 2018 Mahesh Mavurapu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for But.
+FOUNDATION_EXPORT double ButVersionNumber;
+
+//! Project version string for But.
+FOUNDATION_EXPORT const unsigned char ButVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <But/PublicHeader.h>
+
+
